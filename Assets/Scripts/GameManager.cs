54a133f7affe109bc046b5697b470c32c02d6158
Rafael Ortiz <ortiz5064@gmail.com>
2022-09-23 23:19:26 -0500

    Time

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,15 +24,46 @@ public class GameManager : MonoBehaviour
     public TMP_Text textMoneyPlayer;
     public TMP_Text textMoneyPlayerInBank;
 
+    public float time;
+
+    public float hour;
+    public float day;
+    public float month;
+
     void Start()
     {
-        
+        Time.timeScale = 5;
     }
 
     
     void Update()
     {
+        CountTime();
+        
         textMoneyPlayer.text = "Dinero: $" + moneyPlayer.ToString();
         textMoneyPlayerInBank.text = "Banco: $" + moneyPlayerInBank.ToString();
+    }
+
+    void CountTime()
+    {
+        time += Time.deltaTime;
+        
+        if (time >= 40)
+        {
+            hour++;
+            if (hour >= 24)
+            {
+                hour = 0;
+                day++;
+
+                if (day >= 30)
+                {
+                    day = 0;
+                    month++;
+                }
+            }
+
+            time = 0;
+        }
     }
 }
