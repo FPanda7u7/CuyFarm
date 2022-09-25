0a2ae273a4f55502c95585acdd850fd0ac9c7e26
GabrielaDeRojas <gabriela.derojas@tecsup.edu.pe>
2022-09-25 10:57:54 -0500

    Player Stats

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,10 +23,6 @@ public class GameManager : MonoBehaviour
     public int moneyPlayer;
     public int moneyPlayerInBank;
 
-    public int Sueño;
-    public int Hambre;
-    public float tempHoras;
-
     public TMP_Text textMoneyPlayer;
     public TMP_Text textMoneyPlayerInBank;
 
@@ -44,14 +40,6 @@ public class GameManager : MonoBehaviour
 
     public TMP_Text _hours;
     public TMP_Text _days;
-
-    public float time;
-    public float fill1;
-    public float fill2;
-    public Color barraColor;
-
-    public Image barHambre;
-    public Image barSueño;
 
     public string[] days = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
 
@@ -72,17 +60,6 @@ public class GameManager : MonoBehaviour
 
         textMoneyPlayer.text = "Dinero: $" + moneyPlayer.ToString();
         textMoneyPlayerInBank.text = "Banco: $" + moneyPlayerInBank.ToString();
-
-        if (hour > tempHoras)
-        {
-            this.fill1 = Mathf.Clamp01(this.fill1 - 0.1f);
-            this.barSueño.fillAmount = this.fill1;
-            this.fill2 = Mathf.Clamp01(this.fill2 - 0.2f);
-            this.barHambre.fillAmount = this.fill2;
-            tempHoras = hour;
-            CheckColor(barSueño);
-            CheckColor(barHambre);
-        }
     }
 
     void CountTime()
@@ -103,21 +80,5 @@ public class GameManager : MonoBehaviour
         _hours.text = string.Format("{0:00}:00", (hour % 24));
         //_hours.text = (hour % 24).ToString();
         _days.text = days[(int)(day % 7)];
-    }
-    public void CheckColor(Image barra)
-    {
-        if(barra.fillAmount >= 0.7)
-        {
-            this.barraColor = Color.green;
-        }
-        if(barra.fillAmount > 0.4 && barra.fillAmount <= 0.6)
-        {
-            this.barraColor = Color.yellow;
-        }
-        if(barra.fillAmount <= 0.4)
-        {
-            this.barraColor = Color.red;
-        }
-        barra.color = barraColor;
     }
 }
--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
@@ -7,6 +8,16 @@ public class PlayerStats : MonoBehaviour
     public List<Cuy> CuyesList;
     
     public  int _money;
+    public int Sueño;
+    public int Hambre;
+    public int tempHoras;
+    public int _hour;
+    public float time;
+    public float fill1;
+    public float fill2;
+    public Color barraColor;
+    public Image barHambre;
+    public Image barSueño;
 
     void Start()
     {
@@ -18,5 +29,32 @@ public class PlayerStats : MonoBehaviour
     void Update()
     {
         _money = GameManager.instance.moneyPlayer;
+        _hour = GameManager.instance._hour;
+        if (_hour > tempHoras)
+        {
+            this.fill1 = Mathf.Clamp01(this.fill1 - 0.1f);
+            this.barSueño.fillAmount = this.fill1;
+            this.fill2 = Mathf.Clamp01(this.fill2 - 0.2f);
+            this.barHambre.fillAmount = this.fill2;
+            CheckColor(barSueño);
+            CheckColor(barHambre);
+        }
+        tempHoras = _hour;
+    }
+    public void CheckColor(Image barra)
+    {
+        if (barra.fillAmount >= 0.7)
+        {
+            this.barraColor = Color.green;
+        }
+        if (barra.fillAmount > 0.4 && barra.fillAmount <= 0.6)
+        {
+            this.barraColor = Color.yellow;
+        }
+        if (barra.fillAmount <= 0.4)
+        {
+            this.barraColor = Color.red;
+        }
+        barra.color = barraColor;
     }
 }
