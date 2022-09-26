c919bbc328116c45b5231c2e0408f89d9d9d5d71
Rafael Ortiz <ortiz5064@gmail.com>
2022-09-25 19:52:04 -0500

    Tienda Cuy Terminado

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -8,42 +8,51 @@ public class Shop : MonoBehaviour
     public PlayerStats player;
 
     public GameObject foodCuy;
+    public GameObject foodCuyEspecial;
     public GameObject foodPlayer;
 
     private Object _foodCuy;
+    private Object _foodCuyEspecial;
     private Object _foodPlayer;
 
     public int countFoodCuy;
+    public int countFoodCuyEspecial;
     public int countFoodPlayer;
     public int countTotal;
 
+    public float interes;
+
     public TMP_Text textTotal;
     public TMP_Text textFoodCuy;
+    public TMP_Text textFoodCuyEspecial;
     public TMP_Text textFoodPlayer;
 
     public TMP_Text dineroPlayer;
     public TMP_Text dineroDebito;
     public TMP_Text dineroCredito;
+    public TMP_Text dineroCreditoAumento;
 
     void Start()
     {
         _foodCuy = foodCuy.GetComponent<Object>();
+        _foodCuyEspecial = foodCuyEspecial.GetComponent<Object>();
         _foodPlayer = foodPlayer.GetComponent<Object>();
     }
 
     
     void Update()
     {
-        countTotal = (_foodCuy.cost * countFoodCuy) + (_foodPlayer.cost * countFoodPlayer);
+        countTotal = (_foodCuy.cost * countFoodCuy) + (_foodPlayer.cost * countFoodPlayer) + (_foodCuyEspecial.cost * countFoodCuyEspecial);
 
         textTotal.text = "Total: $" + countTotal.ToString();
         textFoodCuy.text = countFoodCuy.ToString();
+        textFoodCuyEspecial.text = countFoodCuyEspecial.ToString();
         textFoodPlayer.text = countFoodPlayer.ToString();
 
         dineroPlayer.text = "$" + GameManager.instance.dineroEfectivo.ToString();
         dineroDebito.text = "$" + GameManager.instance.dineroDebito.ToString();
         dineroCredito.text = "$" + GameManager.instance.dineroCredito.ToString();
-
+        dineroCreditoAumento.text = "$" + (((int)(countTotal * interes)) - countTotal).ToString();
     }
 
     public void AgregarAlCarrito(string comida)
@@ -51,6 +60,11 @@ public class Shop : MonoBehaviour
         if (comida == "Comida Cuy")
         {
             countFoodCuy = Mathf.Clamp(countFoodCuy + 1, 0, 100);
+        }
+
+        if (comida == "Comida Cuy Especial")
+        {
+            countFoodCuyEspecial = Mathf.Clamp(countFoodCuyEspecial + 1, 0, 100);
         }
 
         if (comida == "Comida Player")
@@ -65,6 +79,11 @@ public class Shop : MonoBehaviour
         {
             countFoodCuy = Mathf.Clamp(countFoodCuy - 1, 0, 100);
         }
+
+        if (comida == "Comida Cuy Especial")
+        {
+            countFoodCuyEspecial = Mathf.Clamp(countFoodCuyEspecial - 1, 0, 100);
+        }
         
         if (comida == "Comida Player")
         {
@@ -78,12 +97,17 @@ public class Shop : MonoBehaviour
         {
             player.countFoodCuy++;
         }
+        for (int i = 0; i < countFoodCuyEspecial; i++)
+        {
+            player.countFoodEspecial++;
+        }
         for (int i = 0; i < countFoodPlayer; i++)
         {
             player.countFoodPlayer++;
         }
         countTotal = 0;
         countFoodCuy = 0;
+        countFoodCuyEspecial = 0;
         countFoodPlayer = 0;
     }
 
@@ -107,6 +131,7 @@ public class Shop : MonoBehaviour
 
     public void PagarCredito()
     {
-
+        GameManager.instance.dineroCredito += (int)(countTotal * interes);
+        AgregarInventarioPlayer();
     }
 }
